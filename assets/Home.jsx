0998d2a2d1d062dfe64a4f465d4dad911742e0db
addibushman <addibush@byu.edu>
2024-11-15 03:29:37 -0700

    ok got the home page to kinda work now

--- assets/Home.jsx
+++ assets/Home.jsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div>
       <h1>Welcome to As If Wardrobe</h1>
-      <p>Your '90s-inspired digital closet</p>
+      <p>Your '90s inspired digital closet</p>
     </div>
   );
 };
