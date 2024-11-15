f2d0c362e681f84e8c4ba899c19112769467b522
addibushman <addibush@byu.edu>
2024-11-15 03:32:50 -0700

    jk now it works, accidentally deleted it

--- assets/Home.jsx
+++ assets/Home.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Welcome to As If Wardrobe</h1>
-      <p>Your '90s inspired digital closet</p>
+    <div className="leopard-bg">
+      <main className="container-fluid text-center">
+        <div className="content-section">
+          <h1>Welcome to As If Wardrobe</h1>
+          <p>Your '90s inspired digital closet</p>
+          <form method="get" action="wardrobe.html">
+            <div className="input-group mb-3">
+              <span className="input-group-text">@</span>
+              <input
+                className="form-control"
+                type="text"
+                id="email"
+                placeholder="email@email.com"
+              />
+            </div>
+            <div className="input-group mb-3">
+              <span className="input-group-text">🔒</span>
+              <input
+                className="form-control"
+                type="password"
+                id="password"
+                placeholder="password"
+              />
+            </div>
+            <div className="d-flex justify-content-between">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+              <button type="submit" className="btn btn-secondary">
+                Create Account
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
     </div>
   );
 };
